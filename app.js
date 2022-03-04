2d038c9498105a940cd5218c69f728746c582989
zaryab <zaryabsheikh6@gmail.com>
2022-03-04 12:45:02 +0500

    Adding static and dynamic numbers

---
+++ app.js
@@ -0,0 +1,14 @@
+//this is how we add 2 static numbers
+
+
+let a=10;
+let b=20;
+var c=a + b;
+console.log("The sum of "+a+" and "+b+" is: "+c);
+
+
+
+let x=prompt("Enter first value","");
+let y=prompt("Enter Second  value","");
+var z=parseInt (x) + parseInt (y);
+console.log("The sum of "+x+" and "+y+" is: "+z);
